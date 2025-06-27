c3db1b439fc9df05b005581d438bd8199cbc5524
adhil <adhilp387@gmail.com>
2025-06-27 10:50:44 +0530

    completed daily task

---
+++ 27.js
@@ -0,0 +1,37 @@
+ const two = function (num){
+     
+    return ( num * num );
+ };
+ console.log(two(5));
+ 
+ 
+ 
+ //////////
+ 
+ 
+ const res = ((num)=> num * num );
+    
+     
+    
+  console.log(res(10));
+ 
+
+
+
+
+
+ ////// 
+
+ const main = function (num){
+     
+    return ( num ** 3)
+ };
+ console.log(main(10));
+
+ //// /// 
+
+ const invoke = function (){
+    console.log("welcome");
+    
+ }; invoke();
+ 
